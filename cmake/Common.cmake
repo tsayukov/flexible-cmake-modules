ee1e70578de5d8a2e3e815add72500d43e2eee6e
Pavel Tsayukov <p.tsayukov@gmail.com>
2024-05-15 12:39:34 +0300

    Add `Common.cmake` with basic functionality
    It includes:
    * the `after_project_guard` function;
    * the `no_in_source_builds_guard` macro;
    * the `include_project_module` macro;
    * the definition of the `PROJECT_IS_TOP_LEVEL` for CMake below 3.21.
    
    It also applies `after_project_guard` and `no_in_source_builds_guard` to prevent an in-source build inside the 'cmake' directory.

---
+++ cmake/Common.cmake
@@ -0,0 +1,62 @@
+include_guard(GLOBAL)
+
+
+# Prevent including any listfiles before the `project()` command
+function(after_project_guard)
+  if (NOT (PROJECT_SOURCE_DIR STREQUAL CMAKE_CURRENT_SOURCE_DIR))
+    get_filename_component(file_name "${CMAKE_CURRENT_LIST_FILE}" NAME)
+    message(FATAL_ERROR
+      "\n"
+      "'${file_name}' must be included in the current listfile after the `project()` command.\n"
+    )
+  endif()
+endfunction()
+
+
+# Prevent in-source builds; prefer to start each listfile with this macro
+macro(no_in_source_builds_guard)
+  if (PROJECT_IS_TOP_LEVEL AND (CMAKE_CURRENT_LIST_DIR STREQUAL CMAKE_BINARY_DIR))
+    message(FATAL_ERROR
+      "\n"
+      "In-source builds are not allowed.\n"
+      "Instead, provide a path to build tree like so:\n"
+      "cmake -B <binary-directory>\n"
+      "Or use presets with an out-of-source build configuration like so:\n"
+      "cmake --preset <preset-name>\n"
+      "To remove files you accidentally created execute:\n"
+      "Linux: rm -rf CMakeFiles CMakeCache.txt\n"
+      # TODO: check that this command works well in Windows
+      "Windows: rmdir CMakeFiles /s /q && del /q CMakeCache.txt\n"
+    )
+  endif()
+endmacro()
+
+
+#[=============================================================================[
+  Include the '<module>.cmake' file located in the 'cmake' directory of
+  the current project. It let us include cmake-files by name, preventing
+  name collisions by using `include(<module>)` when a module with
+  the same name is defined in the outer score, e.g. the outer project
+  sets its own `CMAKE_MODULE_PATH`.
+#]=============================================================================]
+macro(include_project_module module)
+  include("${PROJECT_SOURCE_DIR}/cmake/${module}.cmake")
+endmacro()
+
+
+#[=============================================================================[
+  The `PROJECT_IS_TOP_LEVEL` is set by `project()` in CMake 3.21+.
+  Otherwise, the custom version of that variable is used that works in the same
+  way as described in the `PROJECT_IS_TOP_LEVEL` documentation.
+  See: https://cmake.org/cmake/help/latest/variable/PROJECT_IS_TOP_LEVEL.html
+#]=============================================================================]
+if (CMAKE_VERSION LESS 3.21)
+  string(COMPARE EQUAL
+    "${CMAKE_SOURCE_DIR}" "${PROJECT_SOURCE_DIR}"
+    PROJECT_IS_TOP_LEVEL
+  )
+endif()
+
+
+after_project_guard()
+no_in_source_builds_guard()
