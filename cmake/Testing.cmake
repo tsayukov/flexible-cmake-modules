70355d66d7116637b81cb822de1e8933f808ea7b
Pavel Tsayukov <p.tsayukov@gmail.com>
2024-05-20 21:52:35 +0300

    Implement `Testing.cmake`

--- cmake/Testing.cmake
+++ cmake/Testing.cmake
@@ -6,6 +6,37 @@ enable_if_project_variable_is_set(ENABLE_TESTING)
 
 enable_testing()
 
-# TODO: find or fetch GTest
+# TODO: need to force the standard to C++14 here? e.g. by some `cxx_standard_guard`?
+
+#[=============================================================================[
+  TODO(?): specify other testing library if you want so.
+  Otherwise, you may also want to specify a particular version of GTest
+  in the `find_package(GTest <version>)` command and use other URL
+  in the `FetchContent_Declare()` command.
+#]=============================================================================]
+find_package(GTest)
+if (NOT GTest_FOUND)
+  can_install_locally(GTest)
+  include(FetchContent)
+  FetchContent_Declare(
+    GTest
+    URL https://github.com/google/googletest/archive/refs/tags/v1.14.0.zip
+  )
+
+  if (CMAKE_HOST_SYSTEM_NAME STREQUAL "Windows")
+    # Prevent overriding the parent project's compiler/linker settings
+    set(gtest_force_shared_crt ON)
+  endif()
+
+  set(INSTALL_GTEST OFF)
+
+  # TODO(?): set `ON` if testing your project uses GMock
+  set(BUILD_GMOCK OFF)
+
+  FetchContent_MakeAvailable(GTest)
+endif()
+
+# Get the `gtest_add_tests()` and `gtest_discover_tests()` command
+include(GoogleTest)
 
 add_subdirectory(tests)
