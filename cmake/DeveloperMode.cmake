c602bee75d4dfc317213cebd08e016de6c7e3e57
Pavel Tsayukov <p.tsayukov@gmail.com>
2024-05-30 18:17:32 +0300

    Move listfiles with dependencies into corresponding folder

--- cmake/DeveloperMode.cmake
+++ cmake/DeveloperMode.cmake
@@ -2,7 +2,7 @@ include_guard(GLOBAL)
 after_project_guard()
 
 
-include_project_module(Testing)
-include_project_module(Benchmarking)
-include_project_module(Coverage)
-include_project_module(Formatting)
+include_project_module(dependencies/Testing)
+include_project_module(dependencies/Benchmarking)
+include_project_module(dependencies/Coverage)
+include_project_module(dependencies/Formatting)
