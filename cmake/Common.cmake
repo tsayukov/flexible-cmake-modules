440660102d846cebc73ffc93914e8471ab9bdab2
Pavel Tsayukov <p.tsayukov@gmail.com>
2024-06-03 20:35:41 +0300

    Add additional functions and macros

--- cmake/Common.cmake
+++ cmake/Common.cmake
@@ -44,6 +44,9 @@ macro(init_common)
     OFF
   )
 
+  # A place where external dependencies are installed locally
+  set(${PROJECT_NAME_UPPER}_LOCAL_DEPENDENCIES_ROOT "${PROJECT_SOURCE_DIR}/.deps")
+
   # And this guard should be at the end
   no_in_source_builds_guard()
 endmacro()
@@ -124,17 +127,69 @@ macro(enable_if_project_variable_is_set suffix)
 endmacro()
 
 
+# Set `${package}_ROOT` to help `find_package(${package})` find the `package`
+macro(set_local_root_for package)
+  set(${package}_ROOT "${${PROJECT_NAME_UPPER}_LOCAL_DEPENDENCIES_ROOT}")
+endmacro()
+
+
+#[=============================================================================[
+  Populate `package` using the details recorded by an earlier call to
+  the `FetchContent_Declare(${package} <...>)` command.
+#]=============================================================================]
+function(populate_package_locally package)
+  FetchContent_GetProperties(${package})
+  string(TOLOWER ${package} package_lower)
+
+  if (NOT ${package_lower}_POPULATED)
+    message(STATUS "Populate ${package}...")
+
+    FetchContent_Populate(${package})
+
+    if (${package_lower}_POPULATED)
+      message(STATUS "Populate ${package} - done")
+    else()
+      message(FATAL_ERROR "Populate ${package} - failed")
+    endif()
+
+    set(${package_lower}_POPULATED ${${package_lower}_POPULATED} PARENT_SCOPE)
+    set(${package_lower}_SOURCE_DIR ${${package_lower}_SOURCE_DIR} PARENT_SCOPE)
+    set(${package_lower}_BINARY_DIR ${${package_lower}_BINARY_DIR} PARENT_SCOPE)
+  endif()
+endfunction()
+
+
+#[=============================================================================[
+  Run the `execute_process()` command with prepended `message`.
+  All parameters of the `execute_process()` command are passed after `message`.
+  If the executing process fails, then an error occurs. In any case, the end of
+  the process is signaled by the same `message` with the addition of either
+  "done" or "failed".
+#]=============================================================================]
+function(execute_process_with_check message)
+  message(STATUS "${message}...")
+
+  execute_process(${ARGN} RESULT_VARIABLE result)
+
+  if (${result} EQUAL 0)
+    message(STATUS "${message} - done")
+  else()
+    message(FATAL_ERROR "${message} - failed")
+  endif()
+endfunction()
+
+
 # `print` macros for debugging purposes
 macro(print text)
-  message(STATUS "-- ${text}")
+  message(STATUS "--> ${text}")
 endmacro()
 
 macro(print_var variable)
-  message(STATUS "-- ${variable} = ${${variable}}")
+  message(STATUS "--> ${variable} = \"${${variable}}\"")
 endmacro()
 
 macro(print_var_with variable hint)
-  message(STATUS "-- ${hint}: ${variable} = ${${variable}}")
+  message(STATUS "--> ${hint}: ${variable} = \"${${variable}}\"")
 endmacro()
 
 
