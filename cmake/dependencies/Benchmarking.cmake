710bcd0ba9296edbf6211227ba57d03ff72f8758
Pavel Tsayukov <p.tsayukov@gmail.com>
2024-06-12 14:11:40 +0300

    Add google benchmark support

--- cmake/dependencies/Benchmarking.cmake
+++ cmake/dependencies/Benchmarking.cmake
@@ -1,7 +1,36 @@
 include_guard(GLOBAL)
 after_project_guard()
+cxx_standard_guard()
 
 
 enable_if_project_variable_is_set(ENABLE_BENCHMARKING)
 
-# TODO: implement
+#[=============================================================================[
+  TODO(?): specify other benchmarking library if you want so.
+  Otherwise, you may also want to specify a particular version of the google
+  benchmark library in the `find_package(benchmark <version>)` command and use
+  other GIT_TAG in the `FetchContent_Declare()` command.
+#]=============================================================================]
+
+use_cxx_standard_at_least(11)
+
+find_package(benchmark)
+if (NOT benchmark_FOUND)
+  can_install_locally(benchmark)
+  use_cxx_standard_at_least(14)
+
+  include(FetchContent)
+  FetchContent_Declare(
+    benchmark
+    GIT_REPOSITORY https://github.com/google/benchmark
+    GIT_TAG a4cf155615c63e019ae549e31703bf367df5b471 # v1.8.4
+  )
+
+  set(BENCHMARK_ENABLE_INSTALL OFF)
+  set(BENCHMARK_INSTALL_DOCS OFF)
+  set(BENCHMARK_ENABLE_GTEST_TESTS OFF)
+
+  FetchContent_MakeAvailable(benchmark)
+endif()
+
+add_subdirectory(benchmarks)
