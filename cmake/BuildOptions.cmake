8d37dae314c0febc139b1a754d909a3403e97a4e
Pavel Tsayukov <p.tsayukov@gmail.com>
2024-06-14 21:09:41 +0300

    Add 'ENABLE_TREATING_INCLUDES_AS_SYSTEM' option

--- cmake/BuildOptions.cmake
+++ cmake/BuildOptions.cmake
@@ -14,10 +14,7 @@ macro(project_dev_option name help_text)
 endmacro()
 
 
-# TODO(?): For small projects it is useless to enable `ccache`
-project_option(ENABLE_CCACHE "Enable ccache" OFF)
-
-project_option(ENABLE_DOCS "Enable creating documentation" OFF)
+############################### Project options ################################
 
 project_option(ENABLE_DEVELOPER_MODE "Enable developer mode" OFF)
 project_dev_option(ENABLE_TESTING "Enable testing")
@@ -25,3 +22,24 @@ project_dev_option(ENABLE_BENCHMARKING "Enable benchmarking")
 project_dev_option(ENABLE_COVERAGE "Enable code coverage testing")
 project_dev_option(ENABLE_FORMATTING "Enable code formatting")
 # TODO: implement other developer options
+
+project_option(ENABLE_DOCS "Enable creating documentation" OFF)
+
+# TODO(?): For small projects it is useless to enable `ccache`
+project_option(ENABLE_CCACHE "Enable ccache" OFF)
+
+#[=============================================================================[
+  Enable treating the project's include directories as system via passing the
+  `SYSTEM` option to the `target_include_directories` command. This may have
+  effects such as suppressing warnings or skipping the contained headers in
+  dependency calculations (see compiler documentation).
+#]=============================================================================]
+if (NOT PROJECT_IS_TOP_LEVEL)
+  project_option(ENABLE_TREATING_INCLUDES_AS_SYSTEM
+    "Use the `SYSTEM` option for the project's includes; compilers may disable warnings"
+    ON
+  )
+  if (ENABLE_TREATING_INCLUDES_AS_SYSTEM)
+    set(warning_guard SYSTEM)
+  endif()
+endif()
