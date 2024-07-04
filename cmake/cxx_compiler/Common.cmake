9a6eaf52d07d79587b991e530a9ef240811bd177
Pavel Tsayukov <p.tsayukov@gmail.com>
2024-07-04 11:47:55 +0300

    Add 'get_compiler_version' function
    Also rearrange functions, add an additional separator

--- cmake/cxx_compiler/Common.cmake
+++ cmake/cxx_compiler/Common.cmake
@@ -4,6 +4,8 @@ no_in_source_builds_guard()
 variable_init_guard()
 
 
+################################ C++ standard ##################################
+
 #[=============================================================================[
   Check if `CMAKE_CXX_STANDARD` is set to at least `standard` and in the very
   first call define the `${cxx_standard}` target as an interface library with
@@ -29,26 +31,6 @@ macro(use_cxx_standard_at_least standard)
     set(CMAKE_CXX_STANDARD_REQUIRED ON)
   endif()
 endmacro()
-
-# Helper comparison
-function(cmake_cxx_standard_is_not_less_than value)
-  set(lhs ${CMAKE_CXX_STANDARD})
-  set(rhs ${value})
-
-  if (lhs EQUAL "98")
-    set(lhs 9)
-  endif()
-  if (rhs EQUAL "98")
-    set(rhs 9)
-  endif()
-
-  if (lhs LESS rhs)
-    message(FATAL_ERROR
-      "\n"
-      "The library requires the C++ standard ${CMAKE_CXX_STANDARD}. Got: ${value}.\n"
-    )
-  endif()
-endfunction()
 
 # Raise an error if the `${cxx_standard}` target is not defined
 macro(cxx_standard_guard)
@@ -79,3 +61,81 @@ macro(disable_cxx_extensions)
   )
   set(CMAKE_CXX_EXTENSIONS OFF)
 endmacro()
+
+
+######################### Helper functions and macros ##########################
+
+# Helper comparison
+function(cmake_cxx_standard_is_not_less_than value)
+  set(lhs ${CMAKE_CXX_STANDARD})
+  set(rhs ${value})
+
+  if (lhs EQUAL "98")
+    set(lhs 9)
+  endif()
+  if (rhs EQUAL "98")
+    set(rhs 9)
+  endif()
+
+  if (lhs LESS rhs)
+    message(FATAL_ERROR
+      "\n"
+      "The library requires the C++ standard ${CMAKE_CXX_STANDARD}. Got: ${value}.\n"
+    )
+  endif()
+endfunction()
+
+#[=============================================================================[
+  Helper function for optional getting the major, minor, or/and patch version of
+  `${compiler_id}` that has to be the current `${lang}` compiler. The default
+  language is `CXX`. Raise an error if there is no such `${compiler_id}`.
+
+    get_compiler_version(<compiler_id> [MAJOR] [MINOR] [PATCH]
+                         [LANGUAGE <lang>])
+
+  Set a variable called by `${compiler_id}_(major|minor|patch)_version` in
+  a parent scope.
+
+  See supported compilers: https://cmake.org/cmake/help/latest/variable/CMAKE_LANG_COMPILER_ID.html
+  See supported languages: https://cmake.org/cmake/help/latest/command/project.html
+#]=============================================================================]
+function(get_compiler_version compiler_id)
+  set(options MAJOR MINOR PATCH)
+  set(one_value_keywords LANGUAGE)
+  set(multi_value_keywords "")
+  cmake_parse_arguments(PARSE_ARGV 1 "args"
+    "${options}"
+    "${one_value_keywords}"
+    "${multi_value_keywords}"
+  )
+
+  if (NOT args_LANGUAGE)
+    set(args_LANGUAGE CXX)
+  endif()
+
+  if (NOT CMAKE_${args_LANGUAGE}_COMPILER_ID STREQUAL "${compiler_id}")
+    message(FATAL_ERROR
+      "\n"
+      "${compiler_id} is either a wrong name or not the current ${args_LANGUAGE} compiler.\n"
+      "Hint: the current ${args_LANGUAGE} compiler is \"${CMAKE_${args_LANGUAGE}_COMPILER_ID}\".\n"
+    )
+  endif()
+
+  string(REPLACE "." ";" compiler_version_list "${CMAKE_${args_LANGUAGE}_COMPILER_VERSION}")
+  list(LENGTH compiler_version_list size)
+
+  if (args_MAJOR AND (size GREATER_EQUAL "1"))
+    list(GET compiler_version_list 0 compiler_major_version)
+    set(${compiler_id}_major_version "${compiler_major_version}" PARENT_SCOPE)
+  endif()
+
+  if (args_MINOR AND (size GREATER_EQUAL "2"))
+    list(GET compiler_version_list 1 compiler_minor_version)
+    set(${compiler_id}_minor_version "${compiler_minor_version}" PARENT_SCOPE)
+  endif()
+
+  if (args_PATCH AND (size GREATER_EQUAL "3"))
+    list(GET compiler_version_list 2 compiler_patch_version)
+    set(${compiler_id}_patch_version "${compiler_patch_version}" PARENT_SCOPE)
+  endif()
+endfunction()
