1c24cd17830665f57f097b925b4e5591f866a1f6
Pavel Tsayukov <p.tsayukov@gmail.com>
2024-07-10 11:52:07 +0300

    CMake: add watchers for deprecated commands and variables

--- Common.cmake
+++ Common.cmake
@@ -8,6 +8,9 @@
   Commands:
     * Guards:
       - no_in_source_builds_guard
+    * Watchers:
+      - mark_command_as_deprecated (3.18+, see details)
+      - mark_normal_variable_as_deprecated
     * Project related commands:
       - define_project_namespace
       - include_project_module
@@ -122,6 +125,47 @@ function(no_in_source_builds_guard)
     )
   endif()
 endfunction()
+
+
+################################### Watchers ###################################
+
+#[=============================================================================[
+  For CMake version 3.18+, mark the `${command}` command as deprecated because
+  of "${reason}".
+  For earlier versions, a corresponding macro should be written manually after
+  each definition of a deprecated command:
+
+    # E.g. `foo` is a deprecated command now
+    macro(foo)
+      message(DEPRECATION "The `foo` command is deprecated.")
+      _foo(${ARGV})
+    endmacro()
+
+#]=============================================================================]
+macro(mark_command_as_deprecated command reason)
+  cmake_language(EVAL CODE "
+    macro(${command})
+      message(DEPRECATION \"The `${command}` command is deprecated because of: ${reason}.\")
+      _${command}(" [=[ ${ARGV} ]=] ")
+    endmacro()"
+  )
+endmacro()
+
+#[=============================================================================[
+  Mark the `${variable}` normal variable as deprecated because of "${reason}".
+  Even though this macro can be used only for normal variables, since all
+  project cached variables have aliases, these aliases can be used as normal
+  variables in this macro.
+#]=============================================================================]
+macro(mark_normal_variable_as_deprecated variable reason)
+  function(__watcher_deprecated_${variable} variable access)
+    if (access STREQUAL "READ_ACCESS")
+      message(DEPRECATION "The `${variable}` normal variable is deprecated because of: ${reason}")
+    endif()
+  endfunction()
+
+  variable_watch(${variable} __watcher_deprecated_${variable})
+endmacro()
 
 
 ################################################################################
