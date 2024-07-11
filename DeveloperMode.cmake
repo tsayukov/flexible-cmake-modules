69c33d6647164b5f7aa639409dd3265ed54f1205
Pavel Tsayukov <p.tsayukov@gmail.com>
2024-07-11 20:29:35 +0300

    CMake: add descriptions to externals/developer mode modules

--- DeveloperMode.cmake
+++ DeveloperMode.cmake
@@ -1,3 +1,13 @@
+#[=============================================================================[
+  Developer mode dependencies
+  ------------------------------------------------------------------------------
+  It's orthogonal to `Externals.cmake`. These dependencies are only enabled
+  under the developer mode and are not used directly by the current project.
+  It's expected that this listfile will be included after definitions of the
+  project targets, because some of these dependencies may link against the
+  project targets.
+#]=============================================================================]
+
 include_guard(GLOBAL)
 
 
--- Externals.cmake
+++ Externals.cmake
@@ -1,3 +1,13 @@
+#[=============================================================================[
+  External dependencies
+  ------------------------------------------------------------------------------
+  Dependencies in this listfile is not related with those that are enabled
+  under the developer mode. These dependencies are used directly by the current
+  project and usually cannot be excluded. It's expected that this listfile will
+  be included before definitions of the project targets, because some of those
+  targets may link against these dependencies.
+#]=============================================================================]
+
 include_guard(GLOBAL)
 cxx_standard_guard()
 
