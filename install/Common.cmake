99986792c5d343fe1002523a110e610fbc811a82
Pavel Tsayukov <p.tsayukov@gmail.com>
2024-07-12 12:57:42 +0300

    CMake: improve installation rules

--- install/Common.cmake
+++ install/Common.cmake
@@ -26,7 +26,7 @@ include(CMakePackageConfigHelpers)
   Create and install CMake configuration files for using the `find_package`
   command.
 
-    install_cmake_configs(
+    install_project_cmake_configs(
       [PATH_VARS <var1> <var2> ... <varN>]
       [NO_SET_AND_CHECK_MACRO]
       [NO_CHECK_REQUIRED_COMPONENTS_MACRO]
@@ -50,7 +50,7 @@ include(CMakePackageConfigHelpers)
 
   Set `ARCH_INDEPENDENT` for header-only libraries.
 #]=============================================================================]
-function(install_cmake_configs)
+function(install_project_cmake_configs)
   set(options
     NO_SET_AND_CHECK_MACRO
     NO_CHECK_REQUIRED_COMPONENTS_MACRO
@@ -112,11 +112,112 @@ function(install_cmake_configs)
   )
 endfunction()
 
-
-# Install the LICENSE file
-macro(install_license)
+# Install the LICENSE file to `${INSTALL_LICENSE_DIR}`
+macro(install_project_license)
   install(FILES
       "${PROJECT_SOURCE_DIR}/LICENSE"
     DESTINATION "${INSTALL_LICENSE_DIR}"
   )
 endmacro()
+
+#[=============================================================================[
+  Install all headers (i.e. *.h and *.hpp files) located in the `base_directory`
+  and its subdirectories recursively to `${CMAKE_INSTALL_INCLUDEDIR}`.
+  The directory structure in the `base_directory` is copied verbatim to the
+  destination.
+
+    install_project_headers([<base_directory>])
+
+  By default, the `base_directory` parameter is `${PROJECT_SOURCE_DIR}/include`.
+  Set the install component to `${namespace}_headers`.
+#]=============================================================================]
+function(install_project_headers)
+  if (ARGC EQUAL "0")
+    set(base_directory "include")
+  else()
+    set(base_directory "${ARGV0}")
+  endif()
+
+  if (NOT IS_ABSOLUTE "${base_directory}")
+    set(base_directory "${PROJECT_SOURCE_DIR}/${base_directory}")
+  endif()
+
+  install(DIRECTORY
+      "${base_directory}/"
+    DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}"
+    COMPONENT "${namespace}_headers"
+    FILES_MATCHING
+      PATTERN "*.h"
+      PATTERN "*.hpp"
+  )
+endfunction()
+
+#[=============================================================================[
+  Install `targets` to specific destinations (see below). Generate and install
+  `${PACKAGE_EXPORT_TARGET_NAME}.cmake` file.
+
+    install_project_targets(TARGETS <targets>... [HEADER_ONLY])
+
+  `HEADER_ONLY` option is used to exclude installation of other artifacts. It
+  actually doesn't make headers install, only exports the corresponding
+  interface libraries as export targets.
+
+  The specific destinations:
+    RUNTIME DESTINATION: `${CMAKE_INSTALL_BINDIR}`
+    LIBRARY DESTINATION: `${CMAKE_INSTALL_LIBDIR}`
+    ARCHIVE DESTINATION: `${CMAKE_INSTALL_LIBDIR}`
+    INCLUDES DESTINATION: `${CMAKE_INSTALL_INCLUDEDIR}`
+
+  Set the install components to `${namespace}_runtime`,
+  `${namespace}_development`, and `${namespace}_configs`.
+#]=============================================================================]
+function(install_project_targets)
+  set(options HEADER_ONLY)
+  set(one_value_keywords "")
+  set(multi_value_keywords TARGETS)
+  cmake_parse_arguments(PARSE_ARGV 0 "ARGS"
+    "${options}"
+    "${one_value_keywords}"
+    "${multi_value_keywords}"
+  )
+
+  if (NOT ARGS_TARGETS)
+    message(FATAL_ERROR "The `TARGETS` parameters must have at least one target.")
+  endif()
+
+  foreach (target IN LISTS ARGS_TARGETS)
+    if (NOT TARGET ${target})
+      message(FATAL_ERROR "`${target}` is not a target.")
+    endif()
+  endforeach()
+
+  set(artifact_options "")
+  if (NOT ARGS_HEADER_ONLY)
+    list(APPEND artifact_options
+      RUNTIME
+        DESTINATION "${CMAKE_INSTALL_BINDIR}"
+        COMPONENT "${namespace}_runtime"
+      LIBRARY
+        DESTINATION "${CMAKE_INSTALL_LIBDIR}"
+        COMPONENT "${namespace}_runtime"
+        NAMELINK_COMPONENT "${namespace}_development"
+      ARCHIVE
+        DESTINATION "${CMAKE_INSTALL_LIBDIR}"
+        COMPONENT "${namespace}_development"
+    )
+  endif()
+
+  install(TARGETS
+      ${ARGS_TARGETS}
+    EXPORT "${PACKAGE_EXPORT_TARGET_NAME}"
+    ${artifact_options}
+    INCLUDES DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}"
+  )
+
+  install(EXPORT
+      "${PACKAGE_EXPORT_TARGET_NAME}"
+    NAMESPACE ${namespace}::
+    DESTINATION "${INSTALL_CMAKE_DIR}"
+    COMPONENT "${namespace}_configs"
+  )
+endfunction()
