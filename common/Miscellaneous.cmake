9a96c1a8485a3d09ca1788b1dcdb5d52daff61e5
Pavel Tsayukov <p.tsayukov@gmail.com>
2024-09-07 11:30:09 +0300

    Rename 'HostInfo' module to 'Miscellaneous' and add 'get_version' command

--- common/Miscellaneous.cmake
+++ common/Miscellaneous.cmake
@@ -3,7 +3,7 @@
   Distributed under the MIT License. See accompanying file LICENSE or
   https://opensource.org/license/mit for details.
   ------------------------------------------------------------------------------
-  Host information
+  Miscellaneous
   ------------------------------------------------------------------------------
   Commands:
   - get_n_physical_cores
@@ -24,3 +24,14 @@ endmacro()
 macro(get_n_logical_cores)
   cmake_host_system_information(RESULT n_logical_cores QUERY NUMBER_OF_LOGICAL_CORES)
 endmacro()
+
+# Call `${program_path} --version` and put the version into `${output_variable}`
+function(get_version program_path output_variable)
+  execute_process(COMMAND
+      ${program_path} --version
+    OUTPUT_VARIABLE
+      version
+  )
+  string(REGEX MATCH "[0-9]+(\.[0-9]+(\.[0-9]+)?)?" version "${version}")
+  set(${output_variable} ${version} PARENT_SCOPE)
+endfunction()
