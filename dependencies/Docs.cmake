c3feb5db1d09d57736ee582901a119234ccbc8f9
Pavel Tsayukov <p.tsayukov@gmail.com>
2024-09-10 12:14:17 +0300

    Improve 'Docs' module

--- dependencies/Docs.cmake
+++ dependencies/Docs.cmake
@@ -10,6 +10,10 @@
   ------------------------------------------------------------------------------
   Commands:
   - add_docs
+  Init commands:
+  - init_doxygen
+  - generate_doxygen_html_templates
+  - init_doxygen_awesome
   ------------------------------------------------------------------------------
   Usage:
 
@@ -28,6 +32,137 @@ include_guard(GLOBAL)
 
 
 enable_if(ENABLE_DOCS)
+
+macro(init_doxygen)
+  find_package(Doxygen REQUIRED OPTIONAL_COMPONENTS dot)
+
+  # Get the path to the executable file
+  get_target_property(DOXYGEN_EXECUTABLE Doxygen::doxygen IMPORTED_LOCATION)
+
+  # Configuration
+  # See: https://www.doxygen.nl/manual/config.html
+
+  # Project related configuration options
+  set(DOXYGEN_OUTPUT_DIRECTORY "${PROJECT_BINARY_DIR}/docs")
+
+  # Build related configuration options
+  set(DOXYGEN_EXTRACT_ALL YES)
+
+  # Configuration options related to the input files
+  set(DOXYGEN_RECURSIVE YES)
+  set(DOXYGEN_USE_MDFILE_AS_MAINPAGE
+    "${PROJECT_SOURCE_DIR}/README.md"
+  )
+
+  # Configuration options related to the HTML output
+  set(DOXYGEN_GENERATE_TREEVIEW YES)
+  set(DOXYGEN_DISABLE_INDEX NO)
+  set(DOXYGEN_FULL_SIDEBAR NO)
+  set(DOXYGEN_HTML_COLORSTYLE "LIGHT")
+
+  # Configuration options related to diagram generator tools
+  if (Doxygen_dot_FOUND)
+    set(DOXYGEN_HAVE_DOT YES)
+    set(DOXYGEN_DOT_IMAGE_FORMAT "svg")
+    set(DOXYGEN_DOT_TRANSPARENT YES)
+  endif()
+endmacro()
+
+function(generate_doxygen_html_templates)
+  __compact_parse_arguments(
+    __values
+      BASE_DIRECTORY
+      HEADER
+      FOOTER
+      CUSTOM_CSS
+  )
+
+  if (NOT ARGS_BASE_DIRECTORY)
+    set(ARGS_BASE_DIRECTORY "${PROJECT_BINARY_DIR}/docs/template")
+  endif()
+  file(MAKE_DIRECTORY "${ARGS_BASE_DIRECTORY}")
+
+  if (NOT ARGS_HEADER)
+    set(ARGS_HEADER "header.html")
+  endif()
+
+  if (NOT ARGS_FOOTER)
+    set(ARGS_FOOTER "footer.html")
+  endif()
+
+  if (NOT ARGS_CUSTOM_CSS)
+    set(ARGS_CUSTOM_CSS "customdoxygen.html")
+  endif()
+
+  execute_process(COMMAND
+      ${DOXYGEN_EXECUTABLE} -w html
+        ${ARGS_HEADER}
+        ${ARGS_FOOTER}
+        ${ARGS_CUSTOM_CSS}
+    WORKING_DIRECTORY "${ARGS_BASE_DIRECTORY}"
+    ERROR_VARIABLE doxygen_generation_error
+  )
+  if (doxygen_generation_error)
+    message(FATAL_ERROR
+      "An error occurred while generating Doxygen template files: "
+      "${doxygen_generation_error}"
+    )
+  endif()
+
+  set(DOXYGEN_HTML_HEADER
+    "${ARGS_BASE_DIRECTORY}/${ARGS_HEADER}"
+  )
+
+  set(DOXYGEN_TEMPLATE_DIR "${ARGS_BASE_DIRECTORY}" PARENT_SCOPE)
+endfunction()
+
+macro(init_doxygen_awesome)
+  find_path(doxygen_awesome_SOURCE_DIR
+      doxygen-awesome.css
+    PATH_SUFFIXES
+      share/doxygen-awesome-css
+  )
+  mark_as_advanced(doxygen_awesome_SOURCE_DIR)
+  if (NOT doxygen_awesome_SOURCE_DIR)
+    can_install_locally(doxygen_awesome)
+    include(FetchContent)
+    FetchContent_Declare(doxygen_awesome
+      GIT_REPOSITORY https://github.com/jothepro/doxygen-awesome-css
+      GIT_TAG 40e9b25b6174dd3b472d8868f63323a870dfeeb8 # v2.3.3
+    )
+    FetchContent_MakeAvailable(doxygen_awesome)
+  endif()
+
+  # Configuration
+
+  set(DOXYGEN_HTML_EXTRA_STYLESHEET
+    "${doxygen_awesome_SOURCE_DIR}/doxygen-awesome.css"
+    "${doxygen_awesome_SOURCE_DIR}/doxygen-awesome-sidebar-only.css"
+    "${doxygen_awesome_SOURCE_DIR}/doxygen-awesome-sidebar-only-darkmode-toggle.css"
+  )
+
+  # See: https://jothepro.github.io/doxygen-awesome-css/md_docs_2extensions.html#extension-dark-mode-toggle
+  set(DOXYGEN_HTML_EXTRA_FILES
+    "${doxygen_awesome_SOURCE_DIR}/doxygen-awesome-darkmode-toggle.js"
+  )
+
+  if (NOT DEFINED DOXYGEN_TEMPLATE_DIR)
+    message(FATAL_ERROR
+      "`DOXYGEN_TEMPLATE_DIR` is not defined, "
+      "call the `generate_doxygen_html_templates` command before."
+    )
+  endif()
+
+  file(APPEND "${DOXYGEN_TEMPLATE_DIR}/header.html"
+[=[
+<!-- Dark Mode Toggle -->
+<script type="text/javascript" src="$relpath^doxygen-awesome-darkmode-toggle.js"></script>
+<script type="text/javascript">
+  DoxygenAwesomeDarkModeToggle.init()
+</script>
+]=]
+  )
+endmacro()
 
 #[=============================================================================[
   Add the target for building the documentation of files that can be recursively
@@ -73,116 +208,21 @@ function(add_docs target_suffix)
     COMMENT "Generate ${ARGS_FORMAT} documentation"
   )
 
-  enable_if(ENABLE_INSTALL)
-
-  set(docs_component "${namespace}_docs")
-  install(DIRECTORY
-      "${output}"
-    DESTINATION
-      "${INSTALL_DOC_DIR}"
-    COMPONENT ${docs_component} EXCLUDE_FROM_ALL
-  )
-  add_component_target(${docs_component})
+  if(ENABLE_INSTALL)
+    set(docs_component "${namespace}_docs")
+    install(DIRECTORY
+        "${output}"
+      DESTINATION
+        "${INSTALL_DOC_DIR}"
+      COMPONENT ${docs_component} EXCLUDE_FROM_ALL
+    )
+    add_component_target(${docs_component})
+  endif()
 endfunction()
 
 
-################## Initialization the documentation generator ##################
+############################# The end of the file ##############################
 
-find_package(Doxygen REQUIRED OPTIONAL_COMPONENTS dot)
-# Get the path to the executable file
-get_target_property(doxygen Doxygen::doxygen IMPORTED_LOCATION)
-
-find_path(doxygen_awesome_SOURCE_DIR
-    doxygen-awesome.css
-  PATH_SUFFIXES
-    share/doxygen-awesome-css
-)
-mark_as_advanced(doxygen_awesome_SOURCE_DIR)
-if (NOT doxygen_awesome_SOURCE_DIR)
-  can_install_locally(doxygen_awesome)
-  include(FetchContent)
-  FetchContent_Declare(doxygen_awesome
-    GIT_REPOSITORY https://github.com/jothepro/doxygen-awesome-css
-    GIT_TAG 40e9b25b6174dd3b472d8868f63323a870dfeeb8 # v2.3.3
-  )
-  FetchContent_MakeAvailable(doxygen_awesome)
-endif()
-
-
-######################### Doxygen template generation ##########################
-
-set(doxygen_template_header "header.html")
-set(doxygen_template_footer "footer.html")
-set(doxygen_template_custom_css "customdoxygen.html")
-set(doxygen_template_dir "${PROJECT_BINARY_DIR}/docs/template")
-file(MAKE_DIRECTORY "${doxygen_template_dir}")
-
-execute_process(COMMAND
-    ${doxygen} -w html
-      ${doxygen_template_header}
-      ${doxygen_template_footer}
-      ${doxygen_template_custom_css}
-  WORKING_DIRECTORY "${doxygen_template_dir}"
-  ERROR_VARIABLE doxygen_generation_error
-)
-if (doxygen_generation_error)
-  message(FATAL_ERROR
-    "\n"
-    "An error occurred while generating Doxygen template files.\n"
-    "${doxygen_generation_error}\n"
-  )
-endif()
-
-
-############################ Configuration options #############################
-
-# See: https://www.doxygen.nl/manual/config.html
-
-# Project related configuration options
-set(DOXYGEN_OUTPUT_DIRECTORY "${PROJECT_BINARY_DIR}/docs")
-
-# Build related configuration options
-set(DOXYGEN_EXTRACT_ALL YES)
-
-# Configuration options related to the input files
-set(DOXYGEN_RECURSIVE YES)
-set(DOXYGEN_USE_MDFILE_AS_MAINPAGE
-  "${PROJECT_SOURCE_DIR}/README.md"
-)
-
-#Configuration options related to the HTML output
-set(DOXYGEN_GENERATE_TREEVIEW YES)
-set(DOXYGEN_DISABLE_INDEX NO)
-set(DOXYGEN_FULL_SIDEBAR NO)
-set(DOXYGEN_HTML_COLORSTYLE "LIGHT")
-
-set(DOXYGEN_HTML_HEADER
-  "${doxygen_template_dir}/${doxygen_template_header}"
-)
-
-set(DOXYGEN_HTML_EXTRA_STYLESHEET
-  "${doxygen_awesome_SOURCE_DIR}/doxygen-awesome.css"
-  "${doxygen_awesome_SOURCE_DIR}/doxygen-awesome-sidebar-only.css"
-  "${doxygen_awesome_SOURCE_DIR}/doxygen-awesome-sidebar-only-darkmode-toggle.css"
-)
-
-# See: https://jothepro.github.io/doxygen-awesome-css/md_docs_2extensions.html#extension-dark-mode-toggle
-set(DOXYGEN_HTML_EXTRA_FILES
-  "${doxygen_awesome_SOURCE_DIR}/doxygen-awesome-darkmode-toggle.js"
-)
-file(APPEND "${doxygen_template_dir}/header.html"
-[=[
-<!-- Dark Mode Toggle -->
-<script type="text/javascript" src="$relpath^doxygen-awesome-darkmode-toggle.js"></script>
-<script type="text/javascript">
-  DoxygenAwesomeDarkModeToggle.init()
-</script>
-]=]
-)
-
-# Configuration options related to diagram generator tools
-if (Doxygen_dot_FOUND)
-  set(DOXYGEN_HAVE_DOT YES)
-  set(DOXYGEN_DOT_IMAGE_FORMAT "svg")
-  set(DOXYGEN_DOT_TRANSPARENT YES)
-endif()
+init_doxygen()
+generate_doxygen_html_templates()
+init_doxygen_awesome()
