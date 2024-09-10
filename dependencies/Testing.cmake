151fae28c3908564548daf5a85173c2843892f36
Pavel Tsayukov <p.tsayukov@gmail.com>
2024-09-10 12:19:45 +0300

    Improve 'Testing' module

--- dependencies/Testing.cmake
+++ dependencies/Testing.cmake
@@ -12,6 +12,9 @@
   Enable the `${NAMESPACE}_ENABLE_DEVELOPER_MODE` or
   `${NAMESPACE}_ENABLE_TESTING` project option to turn testing on.
   See `../Variables.cmake` for details.
+  ------------------------------------------------------------------------------
+  Init commands:
+  - init_google_test
 #]=============================================================================]
 
 include_guard(GLOBAL)
@@ -19,33 +22,38 @@ include_guard(GLOBAL)
 
 enable_if(ENABLE_TESTING)
 
-enable_testing()
+macro(init_google_test)
+  enable_testing()
 
-use_cxx_standard_at_least(14 NAME "GTest")
+  use_cxx_standard_at_least(14 NAME "GTest")
 
-find_package(GTest)
-if (NOT GTest_FOUND)
-  can_install_locally(GTest)
+  find_package(GTest)
+  if (NOT GTest_FOUND)
+    can_install_locally(GTest)
 
-  include(FetchContent)
-  FetchContent_Declare(
-    GTest
-    GIT_REPOSITORY https://github.com/google/googletest
-    GIT_TAG f8d7d77c06936315286eb55f8de22cd23c188571 # v1.14.0
-  )
+    include(FetchContent)
+    FetchContent_Declare(
+      GTest
+      GIT_REPOSITORY https://github.com/google/googletest
+      GIT_TAG f8d7d77c06936315286eb55f8de22cd23c188571 # v1.14.0
+    )
 
-  # Prevent overriding the parent project's compiler/linker settings
-  set(gtest_force_shared_crt ON)
+    # Prevent overriding the parent project's compiler/linker settings
+    set(gtest_force_shared_crt ON)
 
-  set(INSTALL_GTEST OFF)
+    set(INSTALL_GTEST OFF)
 
-  # Set `ON` if testing your project uses GMock
-  set(BUILD_GMOCK OFF)
+    # Set `ON` if testing your project uses GMock
+    set(BUILD_GMOCK OFF)
 
-  FetchContent_MakeAvailable(GTest)
-endif(NOT GTest_FOUND)
+    FetchContent_MakeAvailable(GTest)
+  endif(NOT GTest_FOUND)
 
-# Get the `gtest_add_tests()` and `gtest_discover_tests()` command
-include(GoogleTest)
+  # Get the `gtest_add_tests()` and `gtest_discover_tests()` command
+  include(GoogleTest)
+endmacro()
 
+############################# The end of the file ##############################
+
+init_google_test()
 add_subdirectory(${TEST_DIR})
