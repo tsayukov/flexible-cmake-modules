3542c195bc1ce3fe7271e78ccd7ed8940aabf661
Pavel Tsayukov <p.tsayukov@gmail.com>
2024-10-30 13:34:53 +0300

    Add basic structure of testing tools

---
+++ __tests__/cmake/TestUtils.cmake
@@ -0,0 +1,41 @@
+# Flexible CMake Modules
+# ------------------------------------------------------------------------------
+# Author: Pavel Tsayukov
+# Repository: https://github.com/tsayukov/flexible-cmake-modules
+# Distributed under the MIT License. See the accompanying file LICENSE or
+# https://opensource.org/license/mit for details.
+# ------------------------------------------------------------------------------
+#
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# FOR COAUTHORS AND CONTRIBUTORS: fill in your name, contacts, and changes above
+#
+#[=============================================================================[
+  Helper commands for testing CMake modules
+#]=============================================================================]
+
+include_guard(GLOBAL)
+
+
+function(test_add_cmake_module_test)
+  # TODO: impl
+endfunction()
+
+macro(test_internal_include_template)
+  # TODO: impl
+endmacro()
+
+macro(test_enable_catch_fatal_error)
+  set(__FCM_DEBUG_CATCH_FATAL_ERROR__ "__FCM_DEBUG_CATCH_FATAL_ERROR__")
+endmacro()
+
+function(test_pop_error_message)
+  # TODO: impl
+endfunction()
+
+macro(message)
+  # TODO: impl
+endmacro()
+
+################################ Init variables ################################
+
+# TODO: impl
